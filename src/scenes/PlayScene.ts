1966d126d36871a6d4dd1f6ebbdb199131db34a1
Nicolas Gauthier <nicolas.gauthier@leadsonline.com>
2025-05-23 10:10:47 -0400

    jump and ground

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,8 +1,16 @@
-import Phaser from "phaser";
+import { Player } from "../entities/Player";
+import { SpriteWithDynamicBody } from "../types";
 
 class PlayScene extends Phaser.Scene {
+  player: Player;
+  ground: Phaser.GameObjects.TileSprite;
+  startTrigger: SpriteWithDynamicBody;
+
   get gameHeight() {
     return this.game.config.height as number;
+  }
+  get gameWidth() {
+    return this.game.config.width as number;
   }
   constructor() {
     super("PlayScene");
@@ -15,14 +23,46 @@ class PlayScene extends Phaser.Scene {
   create() {
     this.createEnvironment();
     this.createPlayer();
+
+    this.startTrigger = this.physics.add
+      .sprite(0, 10, null)
+      .setOrigin(0, 1)
+      .setAlpha(0);
+
+    this.physics.add.overlap(this.player, this.startTrigger, () => {
+      if (this.startTrigger.y === 10) {
+        this.startTrigger.body.reset(0, this.gameHeight);
+        return;
+      }
+
+      this.startTrigger.body.reset(9999, 9999);
+      const rollOutEvent = this.time.addEvent({
+        delay: 1000 / 60,
+        loop: true,
+        callback: () => {
+          this.player.setVelocityX(80);
+          this.ground.width += 17 * 2;
+          if (this.ground.width >= this.gameWidth) {
+            rollOutEvent.remove();
+            this.ground.width = this.gameWidth;
+            this.player.setVelocityX(0);
+          }
+        },
+      });
+    });
   }
 
   createPlayer() {
-    this.physics.add.sprite(0, this.gameHeight, "dino-idle").setOrigin(0, 1);
+    this.player = new Player(this, 0, this.gameHeight);
   }
+
   createEnvironment() {
-    this.add.tileSprite(0, this.gameHeight, 88, 26, "ground").setOrigin(0, 1);
+    this.ground = this.add
+      .tileSprite(0, this.gameHeight, 88, 26, "ground")
+      .setOrigin(0, 1);
   }
+
+  update(time: number, delta: number): void {}
 }
 
 export default PlayScene;
