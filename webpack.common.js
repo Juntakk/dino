eedd81483c42e7d765bff317a9415520acf7f2e0
Nicolas Gauthier <nick.habashi@gmail.com>
2025-06-15 14:01:29 -0400

    fix: add favicon support in HtmlWebpackPlugin and ensure title is correctly placed in index.html

--- webpack.common.js
+++ webpack.common.js
@@ -48,6 +48,7 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: "./index.html",
+      favicon: path.resolve(__dirname, "public/favicon.ico"),
     }),
     new CopyPlugin({
       patterns: [
